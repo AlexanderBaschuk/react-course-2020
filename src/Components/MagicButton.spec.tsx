4a268d3136d87713cc4dc5ebef7ef1ed1e4ceb7b
Alexander Baschuk <a.baschuk@dodopizza.com>
2020-04-12 14:55:23 +0300

    add react tests

--- src/Components/MagicButton.spec.tsx
+++ src/Components/MagicButton.spec.tsx
@@ -1,13 +1,23 @@
-import { shallow } from 'enzyme'
+import { shallow, mount, render } from 'enzyme'
 import React from 'react'
 import { MagicButton } from './MagicButton'
 
 const incrementMock = jest.fn()
 
-test('Default render', () => {
-	expect(
-		shallow(<MagicButton increment={incrementMock} count={2}/>).matchesElement(
-			<button>Button ||</button>,
-		),
-	).toBe(true)
+describe('Magic Button', () => {
+	test('Default render', () => {
+		const button = shallow(<MagicButton increment={incrementMock} count={0} />)
+		expect(button.matchesElement(<button>Button </button>)).toBe(true)
+	})
+
+	test('Render with count', () => {
+		const button = shallow(<MagicButton increment={incrementMock} count={2} />)
+		expect(button.matchesElement(<button>Button ||</button>)).toBe(true)
+	})
+
+	it('should call increment when clicked', () => {
+		const button = shallow(<MagicButton increment={incrementMock} count={2} />)
+		button.find('button').simulate('click')
+		expect(incrementMock).toBeCalledTimes(1)
+	})
 })
