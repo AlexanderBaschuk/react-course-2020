6ef6fae4091de4fa7c81c67cc7bf7a70b2415786
Alexander Baschuk <a.baschuk@dodopizza.com>
2020-04-18 17:54:11 +0300

    Add snapshot tests for cell styles

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,14 @@
 /*eslint-env node*/
 module.exports = {
 	clearMocks: true,
-	coverageDirectory: "coverage",
-	testEnvironment: "jsdom",
-	setupFilesAfterEnv: ["<rootDir>/jestSettings.js"],
+	coverageDirectory: 'coverage',
+	testEnvironment: 'jsdom',
+	setupFilesAfterEnv: ['<rootDir>/jestSettings.js'],
 	transform: {
-	  "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
+		'^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
 	},
-  }
+	moduleNameMapper: {
+		// https://jestjs.io/docs/en/webpack#handling-static-assets
+		'\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+	},
+}
