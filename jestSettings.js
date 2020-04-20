f4be1926d45034cf40d714275386d332092de432
Alexander Baschuk <a.baschuk@dodopizza.com>
2020-04-20 18:28:25 +0300

    fix code style in jest settings file

--- jestSettings.js
+++ jestSettings.js
@@ -1,6 +1,6 @@
-import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-import serializer from "jest-emotion";
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import serializer from 'jest-emotion'
 
-Enzyme.configure({ adapter: new Adapter() });
-expect.addSnapshotSerializer(serializer);
+Enzyme.configure({ adapter: new Adapter() })
+expect.addSnapshotSerializer(serializer)
