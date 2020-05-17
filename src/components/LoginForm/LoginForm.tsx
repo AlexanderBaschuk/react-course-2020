f1d4e6c7c069f33e60ffad76ce84113bd8904fcf
Alexander Baschuk <a.baschuk@dodopizza.com>
2020-05-17 21:11:36 +0300

    Add useLogin hook and sanitize user name

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,26 +1,28 @@
 import React, { useCallback, useState } from 'react'
 
+import { useLogin } from './useLogin'
+
 export interface LoginFormProps {
 	logIn: (name: string) => void
 }
 
 export const LoginForm: React.FC<LoginFormProps> = ({ logIn }) => {
-	const [name, setName] = useState('')
-
-	const changeName = useCallback((event) => {
-		setName(event.target.value)
-	}, [])
+	const { name, changeName } = useLogin()
 
 	const submit = useCallback(() => {
 		event.preventDefault()
 		logIn(name)
 	}, [logIn, name])
 
+	const handleChange = useCallback((event) => {
+		changeName(event.target.value)
+	}, [changeName])
+
 	return (
 		<>
 			<form onSubmit={submit}>
 				<h1>Enter your name</h1>
-				<input type="text" value={name} onChange={changeName}></input>
+				<input type="text" value={name} onChange={handleChange}></input>
 				<input type="submit" value="Log in"></input>
 			</form>
 		</>
