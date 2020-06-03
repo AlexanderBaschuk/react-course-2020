b5ad9e4cbb8d0062a850dd8d92a8d78a1b0f844e
Alexander Baschuk <a.baschuk@dodopizza.com>
2020-06-03 21:01:04 +0300

    Wrap the App into redux provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,26 @@
 import { App } from './App'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import React from 'react'
+import { configureStore } from "@reduxjs/toolkit";
 import { render } from 'react-dom'
 
+const initialState = {}
+
+const rootReducer = (state) => {
+	return state;
+};
+
+const store = configureStore({
+	reducer: rootReducer,
+	preloadedState: initialState,
+});
+
 render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>,
 	document.getElementById('root'),
 )
