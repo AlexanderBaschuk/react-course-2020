265c78bf7b5c39b48cdab6a4fdc48c79d4324906
Alexander Baschuk <a.baschuk@dodopizza.com>
2020-06-05 22:06:14 +0300

    refactor asyncFlow selectors

--- src/App/MiddlewareExercise/AsyncFlow/asyncFlow.selectors.ts
+++ src/App/MiddlewareExercise/AsyncFlow/asyncFlow.selectors.ts
@@ -8,6 +8,12 @@ export const swPeopleSelector = createSelector(
 	(asyncFlow) => asyncFlow.swPeople,
 )
 
-export const isLoadingSelector = (state: IState) => state.asyncFlow.isLoading
+export const isLoadingSelector = createSelector(
+	asyncFlowSelector,
+	(asyncFlow) => asyncFlow.isLoading,
+)
 
-export const errorMessageSelector = (state: IState) => state.asyncFlow.error
+export const errorMessageSelector = createSelector(
+	asyncFlowSelector,
+	(asyncFlow) => asyncFlow.error,
+)
