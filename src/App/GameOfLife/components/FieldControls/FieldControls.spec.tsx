057ace23bd2c3e3ac404b9548a66928866d271ae
Alexander Baschuk <a.baschuk@dodopizza.com>
2020-06-12 23:27:38 +0300

    fix FieldControls tests

--- src/App/GameOfLife/components/FieldControls/FieldControls.spec.tsx
+++ src/App/GameOfLife/components/FieldControls/FieldControls.spec.tsx
@@ -1,72 +1,72 @@
+import { resetAction, resizeAction } from '../../gameOfLife.slice'
+
 import { FieldControls } from '.'
+import { Provider } from 'react-redux'
 import React from 'react'
+import configureStore from 'redux-mock-store'
+import { gameOfLifeInitialState } from '../../gameOfLife.state'
 import { mount } from 'enzyme'
+
+const createStore = () => {
+	const mockStore = configureStore()
+	return mockStore({ gameOfLife: gameOfLifeInitialState })
+}
 
 describe('Field controls', () => {
 	it('should set density 0 when pressing the Clear button', () => {
-		const setDensityMock = jest.fn()
-
+		const store = createStore()
 		const el = mount(
-			<FieldControls /*
-				rowCount={5}
-				colCount={10}
-				setSize={jest.fn()}
-				density={0.5}
-				setDensity={setDensityMock}*/
-			/>,
+			<Provider store={store}>
+				<FieldControls />
+			</Provider>,
 		)
+
 		const clearButton = el.find('div button')
 		clearButton.simulate('click')
-		expect(setDensityMock).toHaveBeenCalledWith(0)
+		expect(store.getActions()).toContainEqual(resetAction(0))
 	})
 
 	it('should call setSize with correct values when changing the size', () => {
-		const setSizeMock = jest.fn()
-
+		const store = createStore()
 		const el = mount(
-			<FieldControls /*
-				rowCount={5}
-				colCount={10}
-				setSize={setSizeMock}
-				density={0.5}
-				setDensity={jest.fn()}*/
-			/>,
+			<Provider store={store}>
+				<FieldControls />
+			</Provider>,
 		)
+
 		const fieldSizeForm = el.find('form').at(0)
 		const rowsInput = fieldSizeForm.find('input[type="text"]').at(0)
-		rowsInput.getDOMNode<HTMLInputElement>().value = '15';
+		rowsInput.getDOMNode<HTMLInputElement>().value = '15'
 		rowsInput.simulate('change')
 
 		const columnsInput = fieldSizeForm.find('input[type="text"]').at(1)
-		columnsInput.getDOMNode<HTMLInputElement>().value = '20';
+		columnsInput.getDOMNode<HTMLInputElement>().value = '20'
 		columnsInput.simulate('change')
 
 		const submitButton = fieldSizeForm.find('input[type="submit"]')
 		submitButton.simulate('submit')
 
-		expect(setSizeMock).toHaveBeenCalledWith(15, 20)
+		expect(store.getActions()).toContainEqual(
+			resizeAction({ rowCount: 15, colCount: 20 }),
+		)
 	})
 
 	it('should call setSpeed when submitting speed change', () => {
-		const densityMock = jest.fn()
-
+		const store = createStore()
 		const el = mount(
-			<FieldControls /*
-				rowCount={5}
-				colCount={10}
-				setSize={jest.fn()}
-				density={0.5}
-				setDensity={densityMock}*/
-			/>,
+			<Provider store={store}>
+				<FieldControls />
+			</Provider>,
 		)
+
 		const densityForm = el.find('form').at(1)
 		const rowsInput = densityForm.find('input[type="text"]')
-		rowsInput.getDOMNode<HTMLInputElement>().value = '0.8';
+		rowsInput.getDOMNode<HTMLInputElement>().value = '0.8'
 		rowsInput.simulate('change')
 
 		const submitButton = densityForm.find('input[type="submit"]')
 		submitButton.simulate('submit')
 
-		expect(densityMock).toHaveBeenCalledWith(0.8)
+		expect(store.getActions()).toContainEqual(resetAction(0.8))
 	})
 })
