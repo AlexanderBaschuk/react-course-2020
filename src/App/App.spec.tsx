346fc3989d52edee2a1e96de32e92300e5f3e0f5
Alexander Baschuk <a.baschuk@dodopizza.com>
2020-07-03 21:20:44 +0300

    extract common helper mountWithWrappers

--- src/App/App.spec.tsx
+++ src/App/App.spec.tsx
@@ -1,24 +1,26 @@
+import { History, createMemoryHistory } from 'history'
+
 import { App } from './App'
 import { Provider } from 'react-redux'
 import React from 'react'
 import { Router } from 'react-router-dom'
-import { createMemoryHistory } from 'history'
+import { Store } from '@reduxjs/toolkit'
 import createMockStore from 'redux-mock-store'
 import { gameOfLifeInitialState } from './GameOfLife/gameOfLife.state'
 import { mount } from 'enzyme'
 
 const store = createMockStore()({ gameOfLife: gameOfLifeInitialState })
+const mountWithWrappers = (jsx: JSX.Element, store: Store, history: History) =>
+	mount(
+		<Provider store={store}>
+			<Router history={history}>{jsx}</Router>
+		</Provider>,
+	)
 
 describe('App routing', () => {
 	it('should initially redirect to login page', () => {
 		const history = createMemoryHistory()
-		const app = mount(
-			<Provider store={store}>
-				<Router history={history}>
-					<App />
-				</Router>
-			</Provider>,
-		)
+		const app = mountWithWrappers(<App />, store, history)
 		expect(history.location.pathname).toBe('/login')
 
 		const prompt = app.find('h1').text()
@@ -28,13 +30,7 @@ describe('App routing', () => {
 	it('should redirect to root after login', () => {
 		const history = createMemoryHistory()
 		history.push('/login')
-		const app = mount(
-			<Provider store={store}>
-				<Router history={history}>
-					<App />
-				</Router>
-			</Provider>,
-		)
+		const app = mountWithWrappers(<App />, store, history)
 
 		const loginInput = app.find('input[type="text"]')
 		loginInput.getDOMNode<HTMLInputElement>().value = 'John'
