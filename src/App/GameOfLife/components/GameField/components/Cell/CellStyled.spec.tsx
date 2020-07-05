460fd7228e7976df4524b533e343cbb4dc806c85
Alexander Baschuk <a.baschuk@dodopizza.com>
2020-07-05 11:18:48 +0300

    remove unused properties of CellStyled

--- src/App/GameOfLife/components/GameField/components/Cell/CellStyled.spec.tsx
+++ src/App/GameOfLife/components/GameField/components/Cell/CellStyled.spec.tsx
@@ -1,6 +1,6 @@
-import renderer from 'react-test-renderer'
 import { CellStyled } from './CellStyled'
 import React from 'react'
+import renderer from 'react-test-renderer'
 
 test.each`
 	cellSize | isAlive
@@ -9,7 +9,7 @@ test.each`
 	${20}    | ${false}
 `('Cell snapshot test', ({ cellSize, isAlive }) => {
 	const cell = renderer
-		.create(<CellStyled cellSize={cellSize} isAlive={isAlive} animate={false} duration={0}/>)
+		.create(<CellStyled cellSize={cellSize} isAlive={isAlive} />)
 		.toJSON()
 	expect(cell).toMatchSnapshot()
 })
