caf566aab3fdd832c2209893a28174634489f35a
Alexander Baschuk <a.baschuk@dodopizza.com>
2020-07-07 22:58:00 +0300

    start writing first puppeteer test

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,12 @@
+/*eslint-env node*/
+module.exports = {
+	launch: {
+		dumpio: true,
+		headless: false,
+		sloMo: 250,
+	},
+	/*server: {
+		command: 'node server.js',
+		port: 4444,
+	},*/
+}
--- src/e2e-tests/gameOfLife.test.ts
+++ src/e2e-tests/gameOfLife.test.ts
@@ -1,3 +1,8 @@
-test('dummy puppeteer test', () => {
-	expect(true).toBeTruthy()
+import { setDefaultOptions } from 'expect-puppeteer'
+
+test('dummy puppeteer test', async () => {
+	const url = 'http://localhost:8080'
+	await page.goto(url)
+	await expect(page.url()).toMatch(/\/login$/)
+	await expect(page).toMatch('Enter your name')
 })
