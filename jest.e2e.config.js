7c0ff8f6b9a4caf9d7d19cbcb74fd1621d3285dc
Alexander Baschuk <a.baschuk@dodopizza.com>
2020-07-07 22:07:45 +0300

    install puppeteer and add jest-puppeteer preset

--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -2,12 +2,13 @@
 module.exports = {
 	clearMocks: true,
 	coverageDirectory: 'coverage',
-	testEnvironment: 'jsdom',
 	moduleNameMapper: {
 		'@/(.*)$': '<rootDir>/src/$1',
 	},
 	transform: {
 		'^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
 	},
+	preset: 'jest-puppeteer',
 	testMatch: ['**/e2e-tests/**/?(*.)+(spec|test).[jt]s?(x)'],
+	verbose: true,
 }
