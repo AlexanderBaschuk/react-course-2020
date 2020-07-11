60ff3efcfb622fa09a63ada99e8e43d8c4c6129e
Alexander Baschuk <a.baschuk@dodopizza.com>
2020-07-11 13:44:30 +0300

    Page object and more e2e tests

--- src/e2e-tests/gameOfLife.test.ts
+++ src/e2e-tests/gameOfLife.test.ts
@@ -1,53 +1,23 @@
-import { ElementHandle } from 'puppeteer'
+import {
+	DEFAULT_COL_COUNT,
+	DEFAULT_ROW_COUNT,
+} from '@/App/GameOfLife/gameOfLife.state'
+import {
+	clickCell,
+	clickClearButton,
+	clickStepButton,
+	getAllCells,
+	getCell,
+	resizeField,
+} from './gameOfLifePage'
+
 import { logIn } from './testsSetup'
-
-interface CellInfo {
-	row: string
-	col: string
-	isAlive: boolean
-}
-
-const cellSelector = '[data-row][data-column]'
-const cellSelectorRowCol = (row: number, col: number) =>
-	`[data-row="${row}"][data-column="${col}"]`
-
-const getLiveness = async (elementHandle: ElementHandle) =>
-	await elementHandle.evaluate((element) => element.getAttribute('data-alive'))
-
-const getRow = async (elementHandle: ElementHandle) =>
-	await elementHandle.evaluate((element) => element.getAttribute('data-row'))
-
-const getColumn = async (elementHandle: ElementHandle) =>
-	await elementHandle.evaluate((element) => element.getAttribute('data-column'))
-
-const toCellInfo = async (cellElement: ElementHandle) => ({
-	row: await getRow(cellElement),
-	col: await getColumn(cellElement),
-	isAlive: (await getLiveness(cellElement)).toLowerCase() === 'true',
-})
-
-const getAllCells = async (): Promise<CellInfo[]> => {
-	const cellElements = await page.$$(cellSelector)
-	const cells = await Promise.all(
-		cellElements.map(async (ce) => await toCellInfo(ce)),
-	)
-	return cells
-}
-
-const getCell = async (row: number, column: number): Promise<CellInfo> => {
-	const cellElement = await page.$(cellSelectorRowCol(row, column))
-	return toCellInfo(cellElement)
-}
-
-const clickCell = async (row: number, column: number): Promise<void> => {
-	await page.click(cellSelectorRowCol(row, column))
-}
 
 beforeEach(async () => {
 	await logIn()
 })
 
-test('at start displays the field having alive and dead cells', async () => {
+test('At start displays the field having live and dead cells', async () => {
 	const cells = await getAllCells()
 	const liveCells = cells.filter((c) => c.isAlive)
 	const deadCells = cells.filter((c) => !c.isAlive)
@@ -55,9 +25,37 @@ test('at start displays the field having alive and dead cells', async () => {
 	expect(deadCells.length).toBeGreaterThan(0)
 })
 
-test('clicking a cell changes its state', async () => {
+test('Clicking a cell changes its state', async () => {
 	const initialCell = await getCell(2, 3)
 	await clickCell(2, 3)
 	const resultCell = await getCell(2, 3)
 	expect(resultCell.isAlive).not.toBe(initialCell.isAlive)
+})
+
+test('Clear button clears the field', async () => {
+	await clickClearButton()
+	const cells = await getAllCells()
+	const liveCells = cells.filter((c) => c.isAlive)
+	expect(liveCells.length).toBe(0)
+})
+
+test('Step button changes the field', async () => {
+	const initialCells = await getAllCells()
+	await clickStepButton()
+	const finalCells = await getAllCells()
+	expect(finalCells).not.toStrictEqual(initialCells)
+})
+
+test('Resize field up', async () => {
+	const initialCells = await getAllCells()
+	await resizeField(DEFAULT_ROW_COUNT + 1, DEFAULT_COL_COUNT + 1)
+	const finalCells = await getAllCells()
+	expect(finalCells.length).toBeGreaterThan(initialCells.length)
+})
+
+test('Resize field down', async () => {
+	const initialCells = await getAllCells()
+	await resizeField(DEFAULT_ROW_COUNT - 1, DEFAULT_COL_COUNT - 1)
+	const finalCells = await getAllCells()
+	expect(finalCells.length).toBeLessThan(initialCells.length)
 })
